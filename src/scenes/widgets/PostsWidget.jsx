32260242ce0ce039a80867018dc123110e33ba59
Nikolai Petrov <104769471+ArdentChimera@users.noreply.github.com>
2023-07-14 15:47:01 +0300

    Update PostsWidget.jsx

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -20,7 +20,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     };
 
     const getUserPosts = async () => {
-        const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
+        const response = await fetch(`https://social-app-backend-aciam0e4t-ardentchimera.vercel.app/posts/${userId}/posts`, {
             method: 'GET',
             headers: { Authorization: `Bearer ${token}` },
         });
